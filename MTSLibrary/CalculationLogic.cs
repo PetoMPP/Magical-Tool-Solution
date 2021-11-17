7a983fa8a89e2670517bfb3a09574997a1d18b14
PetoMPP <piotreq22@gmail.com>
2021-11-17 12:03:40 +0100

    Quick fix of model dependencies

--- MTSLibrary/CalculationLogic.cs
+++ MTSLibrary/CalculationLogic.cs
@@ -10,11 +10,11 @@ namespace MTSLibrary
         public static CompModel CalculateMinimalStock(this CompModel comp)
         {
             //TODO - get comp used tools and max quantity
-            foreach (ToolModel tool in comp.ToolsUsingItem)
-            {
+            
+            
                 //TODO - nonstandard tools - count usage with max comp in tool
                 //TODO - standards - count usage
-            }
+            
             //TODO - clgr multiplier
             return comp;
         }
